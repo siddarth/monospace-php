e5edc3ce1bc2ef0b0cfd937f837af3aeb6d2e62a
Siddarth Chandrasekaran <chandrasekaran.siddarth@gmail.com>
2011-11-19 15:14:45 -0500

    Loop instead of printing one by one.

--- register.php
+++ register.php
@@ -92,46 +92,23 @@
                           <label for="expiry_date">Expiry date *</label>
                           <div class='input'>
                               <select class="small" id="expiry-month" name="expiry-month">
-                                  <option value="1">1</option>
-                                  <option value="2">2</option>
-                                  <option value="3">3</option>
-                                  <option value="4">4</option>
-                                  <option value="5">5</option>
-                                  <option value="6">6</option>
-                                  <option value="7">7</option>
-                                  <option value="8">8</option>
-                                  <option value="9">9</option>
-                                  <option value="10">10</option>
+<?php
+
+  for ($i = 1; $i <= 10; $i++) {
+    echo "\t\t\t\t  <option value=\"$i\">$i</option>\n";
+  }
+?>
                                   <option selected="selected" value="11">11</option>
                                   <option value="12">12</option>
                               </select>
                               <select class="small" id="expiry-year" name="expiry_year">
                                   <option selected="selected" value="2011">2011</option>
-                                  <option value="2012">2012</option>
-                                  <option value="2013">2013</option>
-                                  <option value="2014">2014</option>
-                                  <option value="2015">2015</option>
-                                  <option value="2016">2016</option>
-                                  <option value="2017">2017</option>
-                                  <option value="2018">2018</option>
-                                  <option value="2019">2019</option>
-                                  <option value="2020">2020</option>
-                                  <option value="2021">2021</option>
-                                  <option value="2022">2022</option>
-                                  <option value="2023">2023</option>
-                                  <option value="2024">2024</option>
-                                  <option value="2025">2025</option>
-                                  <option value="2026">2026</option>
-                                  <option value="2027">2027</option>
-                                  <option value="2028">2028</option>
-                                  <option value="2029">2029</option>
-                                  <option value="2030">2030</option>
-                                  <option value="2031">2031</option>
-                                  <option value="2032">2032</option>
-                                  <option value="2033">2033</option>
-                                  <option value="2034">2034</option>
-                                  <option value="2035">2035</option>
-                                  <option value="2036">2036</option>
+<?php
+
+  for ($i = 2012; $i <= 2036; $i++) {
+    echo "\t\t\t\t  <option value=\"$i\">$i</option>\n";
+  }
+?>
                               </select>
 
                           </div>
